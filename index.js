f392a4bf7117878f0b8cbd291a17e814daac56d6
Atharva Todkary <100767857+AtharvaTodkary@users.noreply.github.com>
2024-04-22 14:16:42 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+var fiboArr = [0,1];
+var initVar = 0;
+
+function fibonacciSeries(){
+  for(var i = 0; i<10; i++){
+    // fiboArr[i+2] = fiboArr[i] + fiboArr[i+1];
+    var x = fiboArr[i] + fiboArr[i+1];
+    fiboArr.append(x);
+  }
+}
